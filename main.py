ed7ddab76fe1d47f0459dcd8c80e9cac70bdeebe
Manjinderjit <manjindergrewal1997@gmail.com>
2026-01-30 20:16:34 -0800

    Added user prompt

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import argparse
 from dotenv import load_dotenv
 from google import genai
+
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
@@ -9,15 +11,26 @@ api_key = os.getenv("GEMINI_API_KEY")
 if not api_key:
     raise ValueError("API_KEY not found in environment variables.")
 
+
 def main():
-    print("Hello from ai-agent!" )
-    
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+    print("Hello from ai-agent!")
+
     client = genai.Client(api_key=api_key)
     response_1 = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents='What model are you',
+        model="gemini-2.5-flash", 
+        contents=args.user_prompt,
     )
     
+    if not response_1.usage_metadata:
+        raise RuntimeError("NO RESPONSE!!")
+    
+    print(f'Prompt token: {response_1.usage_metadata.prompt_token_count}')
+    print("Response tokens:", response_1.usage_metadata.candidates_token_count)
+    
+          
     print("Response from Gemini API:", response_1.text)
 
 
