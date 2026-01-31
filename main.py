ef8958072043eab0925bf53483086e018e8d6dc2
Manjinderjit <manjindergrewal1997@gmail.com>
2026-01-31 00:53:39 -0800

    Added calcualator and tests

--- main.py
+++ main.py
@@ -1,36 +1,48 @@
 import os
 import argparse
+
 from dotenv import load_dotenv
 from google import genai
-
-
-load_dotenv()
-api_key = os.getenv("GEMINI_API_KEY")
-
-
-if not api_key:
-    raise ValueError("API_KEY not found in environment variables.")
+from google.genai import types
 
 
 def main():
-    parser = argparse.ArgumentParser(description="Chatbot")
+    parser = argparse.ArgumentParser(description="Gemini CLI Tool")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.getenv("GEMINI_API_KEY")
+    
+    
+    if not api_key:
+        raise ValueError("API_KEY not found in environment variables.")
+
     print("Hello from ai-agent!")
+    
+    
 
     client = genai.Client(api_key=api_key)
-    response_1 = client.models.generate_content(
-        model="gemini-2.5-flash", 
-        contents=args.user_prompt,
-    )
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}\n")
+    generate_content(client, messages, args.verbose)
     
+    
+def generate_content(client, messages, verbose): 
+    response_1 = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=messages,
+    )
+
     if not response_1.usage_metadata:
         raise RuntimeError("NO RESPONSE!!")
-    
-    print(f'Prompt token: {response_1.usage_metadata.prompt_token_count}')
-    print("Response tokens:", response_1.usage_metadata.candidates_token_count)
-    
-          
+
+    if verbose:
+        print(f"Prompt token: {response_1.usage_metadata.prompt_token_count}")
+        print("Response tokens:", response_1.usage_metadata.candidates_token_count)
+
     print("Response from Gemini API:", response_1.text)
 
 
