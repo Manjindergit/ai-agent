6954cea13dac59fa6a64738dd17252048cfd7319
Manjinderjit <manjindergrewal1997@gmail.com>
2026-01-31 22:41:17 -0800

    added testes for write  files

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -17,8 +17,8 @@ def get_file_content(working_directory, file_path):
             file_content_string = f.read(MAX_CHARS)
             # After reading the first MAX_CHARS...
             if f.read(1):
-                content += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
-            return file_content_string
+                file_content_string += f'[...File "{file_path}" truncated at {MAX_CHARS} characters]'
+        return file_content_string
             
     except Exception as e:
         print(f"Exception occurred: {e}")
