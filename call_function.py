93b9b798b11c00b80857b4d770adf3aa3dc560a5
Manjinderjit <manjindergrewal1997@gmail.com>
2026-02-21 21:05:34 -0800

    fixed import dependency in call_function

--- call_function.py
+++ call_function.py
@@ -1,23 +1,25 @@
 from google.genai import types
 
 from config import WORKING_DIR
-from functions.get_file_content import get_file_content, schema_get_file_content
-from functions.get_files_info import get_files_info, schema_get_files_info
-from functions.run_python_file import run_python_file, schema_run_python_file
+from functions.get_files_info import schema_get_files_info, get_files_info
+from functions.get_file_content import schema_get_files_content, get_file_content
 from functions.write_file import schema_write_file, write_file
+from functions.run_python_file import schema_run_file_content, run_python_file
+
+
 
 available_functions = types.Tool(
     function_declarations=[
         schema_get_files_info,
-        schema_get_file_content,
-        schema_run_python_file,
+        schema_get_files_content,
         schema_write_file,
+        schema_run_file_content,
     ]
 )
 
 function_map = {
-    "get_files_info": get_files_info,
     "get_file_content": get_file_content,
+    "get_files_info": get_files_info,
     "run_python_file": run_python_file,
     "write_file": write_file,
 }
